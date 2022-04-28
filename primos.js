4b973238ea49c8b6d2c7a334ff025be6578dc001
Elielder Oliveira <taganet.info@gmail.com>
2022-04-28 19:40:37 -0300

    Create primos.js
    Retorna apenas os números primos entres o intervalo informado

---
+++ primos.js
@@ -0,0 +1,32 @@
+/*
+    Elielder Oliveira
+    Retorna apenas os numeros primos entres o intervalo informado
+*/
+const primos = (numberOne, numberTwo) => {
+    let numberPrimos = new Array;
+    
+    function validPrimo(validNumber)
+    {
+    	let divisores = 0;
+      for(let d = 1; d <= validNumber; d++)
+      {
+      	if((validNumber % d) === 0)
+        {
+        	divisores++;
+        }
+      }
+      return divisores;
+    }
+    
+    for(let i = (numberOne + 1); i < numberTwo; i++ )
+    {
+    	if(validPrimo(i) === 2)
+      {
+     	 numberPrimos.push(i);
+      }
+    }
+    
+    return numberPrimos;
+}
+
+console.log(primos(10, 29));
