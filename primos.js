f0591e82042c03209e0db6ce04f9f0c3f4a2cf15
Elielder Oliveira <taganet.info@gmail.com>
2022-04-28 19:54:45 -0300

    Update primos.js

--- primos.js
+++ primos.js
@@ -3,30 +3,30 @@
     Retorna apenas os numeros primos entres o intervalo informado
 */
 const primos = (numberOne, numberTwo) => {
-    let numberPrimos = new Array;
-    
-    function validPrimo(validNumber)
+  let numberPrimos = new Array;
+  
+  function validPrimo(validNumber)
+  {
+    let divisores = 0;
+    for(let d = 1; d <= validNumber; d++)
     {
-    	let divisores = 0;
-      for(let d = 1; d <= validNumber; d++)
+      if((validNumber % d) === 0)
       {
-      	if((validNumber % d) === 0)
-        {
-        	divisores++;
-        }
-      }
-      return divisores;
-    }
-    
-    for(let i = (numberOne + 1); i < numberTwo; i++ )
-    {
-    	if(validPrimo(i) === 2)
-      {
-     	 numberPrimos.push(i);
+        divisores++;
       }
     }
-    
-    return numberPrimos;
+    return divisores;
+  }
+  
+  for(let i = (numberOne + 1); i < numberTwo; i++ )
+  {
+    if(validPrimo(i) === 2)
+    {
+      numberPrimos.push(i);
+    }
+  }
+  
+  return numberPrimos;
 }
-
-console.log(primos(10, 29));
+let PrimosRes = primos(10, 29);
+console.log("Encontrados "+ PrimosRes.length +" números primos, são eles: "+ PrimosRes);
